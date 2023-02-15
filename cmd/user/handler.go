6589436994f7c273af037724cf98c805c397492f
xiongjiezhu <xiongjiezhu@xiongjiezhu-virtual-machine>
2023-02-15 15:16:54 +0800

    user handler更新

--- cmd/user/handler.go
+++ cmd/user/handler.go
@@ -19,6 +19,43 @@ type UserServiceImpl struct{}
 // UserRegister implements the UserServiceImpl interface.
 func (s *UserServiceImpl) UserRegister(ctx context.Context, req *user.DouyinUserRegisterRequest) (resp *user.DouyinUserRegisterResponse, err error) {
 	// TODO: Your code here...
+
+	username := req.GetUsername()
+	password := req.GetPassword()
+	//随机生成salt
+	salt := tools.RandomStringUtil()
+	//密码加密,内部实现改成了用bcrypt加密
+	password = tools.Md5Util(password, salt)
+
+	//更新用户ID
+	userIdSequence := db_mysql.GetUserService().FindLastUserId()
+
+	//注册用户
+	err = db_mysql.GetUserService().UserRegister(username, password, salt)
+
+	if err != nil {
+		resp.UserId = 0
+		msg := err.Error()
+		resp.BaseResp.StatsuMsg = &msg
+		resp.BaseResp.StatusCode = fail
+
+	} else {
+		token, err := tools.GenToken(username, userIdSequence)
+		if err != nil {
+			msg := "token generation failed" + err.Error()
+			resp.BaseResp.StatsuMsg = &msg
+			resp.BaseResp.StatusCode = fail
+		}
+
+		msg := "UserRegister successfully"
+		atomic.AddInt64(&userIdSequence, 1)
+		resp.UserId = userIdSequence
+		resp.Token = token
+		resp.BaseResp.StatsuMsg = &msg
+		resp.BaseResp.StatusCode = success
+
+	}
+	return resp, err
 	return
 }
 
